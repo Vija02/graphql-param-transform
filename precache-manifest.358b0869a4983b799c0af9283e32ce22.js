fe337a49d8c8ae04882420243e57bdc094d89159
Michael Salim <salim_michaelmi@yahoo.com>
2019-02-01 16:42:07 +0000

    Updates

---
+++ precache-manifest.358b0869a4983b799c0af9283e32ce22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2d5eeeaeb10bec6f3bf",
+    "url": "/static/js/main.c2d5eeea.chunk.js"
+  },
+  {
+    "revision": "d1f511bc4989b3cd7699",
+    "url": "/static/js/1.d1f511bc.chunk.js"
+  },
+  {
+    "revision": "c2d5eeeaeb10bec6f3bf",
+    "url": "/static/css/main.d7ceebd3.chunk.css"
+  },
+  {
+    "revision": "f07e11dd1838f3cc0440fa5d9f6aa9b1",
+    "url": "/index.html"
+  }
+];
